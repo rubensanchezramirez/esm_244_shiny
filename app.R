965a63fd5ff87097223a0b12462e82446b205e28
rubensanchezramirez <rubensanchezramirez@ucsb.edu>
2020-03-10 22:19:51 -0700

    All widgets are up and running

--- app.R
+++ app.R
@@ -22,12 +22,22 @@ library(shinyjs)
 library(DT)
 #library(visNetwork)
 library(rintrojs)
+library(gt)
+
+# ---------------------------------------
+# Support functions:
+# --------------------------------------
 
 # Read in data and create `sf` file:
-fire_mt <- read_sf(here::here("data", "montana_fire_data.csv")) %>% 
+fire_mt <- read_csv(here::here("data", "montana_fire_data.csv")) %>% 
   st_as_sf(coords = c("longitude", "latitude"), crs = 4326)%>% 
-  mutate(fire_year = as.numeric(fire_year)) %>%
-  mutate(fire_name = order(fire_name))
+  mutate(fire_year = as.numeric(fire_year))
+
+# Dateframe with `fire_name` in alphabeticla order for searching:
+fire_name_df <- fire_mt[order(fire_mt$fire_name),]
+
+# Dataframe with `source_reporting_unit_name` in alphabetical order for searching:
+fire_unit_df <- fire_mt[order(fire_mt$source_reporting_unit_name),]
 
 # Color palette for cause map:
 pal <- colorFactor(
@@ -41,8 +51,9 @@ icons <- awesomeIcons(
   library = 'fa',
   markerColor = pal)
 
-#get montana state and counties shapefile 
-montana_counties <- us_counties(states = "Montana")
+# ------------------------------------
+# ui.R
+# -----------------------------------
 
 ui <- navbarPage(title = img(src="Bren_logo.png", height = "34px"),
                  id = "navBar",
@@ -56,10 +67,18 @@ ui <- navbarPage(title = img(src="Bren_logo.png", height = "34px"),
                        }",
                    "body {padding-top: 75px;}"),
                  
+# ----------------------------------------------------------
+# About Panel:
+# -----------------------------------------------------
+                 
                  tabPanel("About", icon = icon("bars")#,
                           #includeMarkdown("www/home.md")
                           ),
                  
+# ---------------------------------------------
+# Fire Cause Panel:
+# -----------------------------------------
+
                  tabPanel("Fires Causes", value = "cause", icon = icon("fire"),
                             sidebarLayout(
                             sidebarPanel(width = 3, align = 'left',
@@ -80,32 +99,61 @@ ui <- navbarPage(title = img(src="Bren_logo.png", height = "34px"),
                             mainPanel(width = 8,
                                       fluidRow(
                                       leafletOutput("cause_map", height = 680))
-                            )
-                            )
-                 ),
-                 
-                 tabPanel("Search by Fire", value = "search", icon = icon("search"),
-                          sidebarLayout(
-                            sidebarPanel(width = 3, align = 'left',
-                                         selectInput(inputId = "mt_fire_name", label = "Choose a Fire", 
-                                                     choices = (fire_mt$fire_name), 
-                                                     selected = "Treasure County Complex"),
-                            ),
-                            mainPanel(width = 8,
-                                      fluidRow(
-                                        plotOutput("fire_map", height = 500)
-                                        )#,
-                                      #fluidRow(
-                                        #gt_output("fire_summary", height = 180)
-                                        #)
                                       )
                             )
                           ),
                  
-                 tabPanel("Containment Time", value = "time", icon = icon("clock"))
+# ---------------------------------------
+# Search by Fire Panel:
+# ---------------------------------------
+
+                 tabPanel("Search by Fire", value = "search", icon = icon("search"),
+                          sidebarLayout(
+                            sidebarPanel(width = 3, align = 'left',
+                                         selectInput(inputId = "mt_fire_name", label = "Choose a Fire", 
+                                                     choices = (fire_name_df$fire_name), 
+                                                     selected = "Treasure County Complex")),
+                            mainPanel(width = 8,
+                                      fluidRow(leafletOutput("fire_map", height = 450)
+                                        ),
+                                      fluidRow(gt_output("fire_summary")
+                                        )
+                                      )
+                            )
+                          ),
                  
-                 )
+# -------------------------------------
+# Containment Time Panel:
+# ---------------------------------------
+
+                 tabPanel("Containment Time", value = "time", icon = icon("clock"),
+                          sidebarLayout(
+                            sidebarPanel(width = 3, align = 'left',
+                                         selectInput("reporting_unit", 
+                                                     "Choose a reporting source:",
+                                                     choices = c(
+                                                       unique(fire_unit_df$source_reporting_unit_name)))
+                                         ),
+                            mainPanel(width = 8,
+                                      fluidRow(
+                                        plotOutput(outputId = "burn_time_plot", height = 680)
+                                        )
+                                      )
+                            )
+                          )
+)
+
+# ---------------------------------------------------------------
+# -------------------------------------------------------------
+# server.R
+# ---------------------------------------------------------------
+# --------------------------------------------------------------
+
 server <- function(input, output){
+  
+# -------------------------------------
+# Cause Map Output:
+# ------------------------------------
   
   filtered_mt_cause <- reactive({
     fire_mt %>%
@@ -137,8 +185,12 @@ server <- function(input, output){
       
   })
   
+# ----------------------------------------
+# Fire Name Output:
+# ---------------------------------------
+  
   fire_name_map <- reactive({
-    fire_mt %>% 
+    fire_name_df %>% 
       filter(fire_name == input$mt_fire_name)
   }) 
   
@@ -153,31 +205,87 @@ server <- function(input, output){
       addLayersControl(
       baseGroups = c("Stamen Terrain", "Open Street Map", "Carto DB Dark Matter", "Esri NatGeo World Map"),
         position = c("topleft"),
-        options = layersControlOptions(collapsed = TRUE)
+      options = layersControlOptions(collapsed = TRUE)
       )
   })
   
-  #mt_gt <- reactive({
-   # mt_date_combined %>%  
-      #select(fire_name, fire_year, stat_cause_descr, fire_size, source_reporting_unit_name, final_dis_date, final_cont_date) %>% 
-      #filter(fire_name == input$mt_fire_name) %>% 
-      #gt() %>% 
-      #tab_header(
-        #title = "Fire Summary") %>% 
-      #cols_label(
-        #fire_name = "Name of Fire",
-        #fire_year = "Year",
-        #stat_cause_descr = "Cause of Fire",
-        #fire_size = "Fire Area",
-        #source_reporting_unit_name = "Name of Reporting Source",
-        #final_dis_date = "Discovery Time",
-        #final_cont_date = "Contained Time") %>% 
-      #tab_options(
-        #table.width = pct(100))
-  #})
+# -------------- Fire Summary ------------
+  
+  fire_name_stats <- reactive({
+    fire_mt %>%  
+      select(fire_name, fire_year, stat_cause_descr, fire_size, source_reporting_unit_name, burn_time) %>%
+      filter(fire_name == input$mt_fire_name) %>% 
+      gt() %>% 
+      tab_header(
+        title = "Fire Summary") %>% 
+      cols_label(
+        fire_name = "Name of Fire",
+        fire_year = "Year",
+        stat_cause_descr = "Cause of Fire",
+        fire_size = "Fire Area (acres)",
+        source_reporting_unit_name = "Name of Reporting Source",
+        burn_time = "Burn Time (D H:M:S)") %>% 
+      tab_options(
+        table.width = pct(100))
+  })
+  
+  output$fire_summary <- render_gt({
+    expr = fire_name_stats()
+  })
+  
+  # ---------------------------------
+  # Burn Time Output:
+  # --------------------------------
+  
+  fire_time <- fire_unit_df %>%
+    mutate(burn_time = difftime(full_cont_date,full_dis_date,units = "mins"))
+  
+  unit_burn_time <- reactive({
+    fire_time %>% 
+      filter(source_reporting_unit_name == input$reporting_unit) %>% 
+      group_by(fire_year) %>% 
+      summarize(
+        mean_burn_time = round(mean(burn_time),2)
+      )
+  })
+  
+  output$burn_time_plot <- renderPlot({
+    ggplot(data = unit_burn_time(), aes(x = fire_year, y = mean_burn_time)) +
+      geom_line(color = "red",
+                size = 1) + 
+      geom_point(size = 2) + 
+      geom_ribbon(aes(ymin = 0, 
+                      ymax = mean_burn_time), 
+                  fill = "red", 
+                  alpha = 0.1) +
+      scale_x_continuous(breaks = seq(1992, 2015, by = 2),
+                         expand = c(0, 0)) + 
+      labs(title = "Montana Fire Containment Times",
+           subtitle = "By Land Management Reporting Agency (1992-2015)",
+           caption = "Time series of how Montana wildfire containment times (in hours) have fluctuated annually between 1992-2015 within different land manangement units.",
+           x = "Year",
+           y = "Containment Time (D H:M:S)") +
+      theme_bw() +
+      theme(axis.text.x = element_text(size = 12),
+            axis.title.y = element_text(size = 12),
+            plot.title = element_text(size = 16),
+            plot.subtitle = element_text(size = 14))
+    
+  })
   
 }
+
+# ----------------------------
+# End of app.R
+# ---------------------------
+
 shinyApp(ui = ui, server = server)
+
+# ------------------------------------------
+# ------------------------------------------
+# END
+# ------------------------------------------
+# ------------------------------------------
 
 
 
